933e0b048c30205c3a0b5243ce72b196e4c8564b
sara khalil <sarakhalil3377@gmail.com>
2022-08-03 14:07:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+
+
+
+
+    let input = document.getElementById('button');
+    input.addEventListener('click', function () {
+        alert('I was clicked!');
+    });
 }
